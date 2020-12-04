5647205c871e1c681a57bb4e1781d23e16b366c0
Joseph Ligier <littel.jo@gmail.com>
2020-12-04 13:56:59 +0000

    Fix hosname

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 
 echo "127.0.0.1 $RABBITMQ_HOSTNAME" > /etc/hosts-rabbitmq
 echo "NODENAME=rabbit@$RABBITMQ_HOSTNAME" > /etc/rabbitmq/rabbitmq-env.conf
+echo "CONFIG_FILE=/etc/rabbitmq/rabbitmq.conf" >> /etc/rabbitmq/rabbitmq-env.conf
+echo "cluster_name = rabbit@$RABBITMQ_HOSTNAME" > /etc/rabbitmq/rabbitmq.conf
 
 /usr/local/bin/docker-entrypoint.sh rabbitmq-server
