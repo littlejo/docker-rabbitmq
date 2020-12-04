c744115380e4fcd368606efd5239507feec586db
Joseph Ligier <littel.jo@gmail.com>
2020-12-04 13:55:43 +0000

    Fix start

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 echo "127.0.0.1 $RABBITMQ_HOSTNAME" > /etc/hosts-rabbitmq
 echo "NODENAME=rabbit@$RABBITMQ_HOSTNAME" > /etc/rabbitmq/rabbitmq-env.conf
 
-/opt/rabbitmq/sbin/rabbitmq-server
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server
